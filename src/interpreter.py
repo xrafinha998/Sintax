9fd97df0ba5dc550d364d40a68c4c9be9bfa5e1c
xrafinha998 <Hakerlega0@gmail.com>
2026-03-22 02:25:12 -0300

    Update interpreter.py

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,1 +1,19 @@
+class Interpreter:
+    def __init__(self):
+        self.memoria = {}
+
+    def rodar(self, nos):
+        for no in nos:
+            if isinstance(no, NoAtribuicao):
+                self.memoria[no.nome] = no.valor
+            
+            elif isinstance(no, NoSe):
+                val_real = self.memoria.get(no.nome_var, 0)
+                resultado = False
+                if no.operador == '>': resultado = val_real > no.valor_comp
+                if no.operador == '<': resultado = val_real < no.valor_comp
+                if no.operador == '==': resultado = val_real == no.valor_comp
+                
+                if resultado:
+                    print(f"Sintax diz: {no.mensagem}")
 
