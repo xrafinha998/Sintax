9d3d32015787a003c72b5843bbccfabfb730d4af
xrafinha998 <Hakerlega0@gmail.com>
2026-03-22 10:07:43 -0300

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -1,16 +1,16 @@
-from src.tokens import T_EOF, T_ID, T_ATR, T_SE, T_STR, T_OP
+from src.tokens import *
+
+class NoExpressao: # Para contas: pontos + 10
+    def __init__(self, esq, op, dir):
+        self.esq, self.op, self.dir = esq, op, dir
 
 class NoAtribuicao:
     def __init__(self, nome, valor):
-        self.nome = nome
-        self.valor = valor
+        self.nome, self.valor = nome, valor
 
 class NoSe:
-    def __init__(self, nome_var, operador, valor_comp, mensagem):
-        self.nome_var = nome_var
-        self.operador = operador
-        self.valor_comp = valor_comp
-        self.mensagem = mensagem
+    def __init__(self, condicao, acao, senao_acao=None):
+        self.condicao, self.acao, self.senao_acao = condicao, acao, senao_acao
 
 class Parser:
     def __init__(self, tokens):
@@ -18,22 +18,31 @@ class Parser:
         self.pos = 0
 
     def parse(self):
-        nos = []
-        # O loop continua enquanto não chegar no fim dos tokens
-        while self.pos < len(self.tokens) and self.tokens[self.pos].tipo != T_EOF:
-            token_atual = self.tokens[self.pos]
+        instrucoes = []
+        while self.tokens[self.pos].tipo != T_EOF:
+            t = self.tokens[self.pos]
             
-            # Caso 1: Atribuição (ex: pontos = 100)
-            if token_atual.tipo == T_ID and (self.pos + 1) < len(self.tokens) and self.tokens[self.pos+1].tipo == T_ATR:
-                nome = token_atual.valor
-                valor = self.tokens[self.pos+2].valor
-                nos.append(NoAtribuicao(nome, int(valor)))
-                self.pos += 3
+            if t.tipo == T_ID and self.tokens[self.pos+1].tipo == T_ATR:
+                nome = t.valor
+                self.pos += 2
+                valor = self.tokens[self.pos].valor
+                # Verifica se é uma conta (ex: x = x + 1)
+                if self.tokens[self.pos+1].tipo in (T_SOMA, T_SUB):
+                    op = self.tokens[self.pos+1].tipo
+                    v2 = self.tokens[self.pos+2].valor
+                    instrucoes.append(NoAtribuicao(nome, NoExpressao(valor, op, v2)))
+                    self.pos += 3
+                else:
+                    instrucoes.append(NoAtribuicao(nome, valor))
+                    self.pos += 1
             
-            # Caso 2: Estrutura SE (ex: se pontos > 50 "Ganhou")
-            elif token_atual.tipo == T_SE:
-                v_nome = self.tokens[self.pos+1].valor
-                op = self.tokens[self.pos+2].valor
+            elif t.tipo == T_SE:
+                cond = (self.tokens[self.pos+1].valor, self.tokens[self.pos+2].valor, self.tokens[self.pos+3].valor)
+                acao = self.tokens[self.pos+4].valor
+                self.pos += 5
+                instrucoes.append(NoSe(cond, acao))
+            else: self.pos += 1
+        return instrucoes
                 v_comp = int(self.tokens[self.pos+3].valor)
                 msg = self.tokens[self.pos+4].valor 
                 nos.append(NoSe(v_nome, op, v_comp, msg))
